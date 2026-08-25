c716a85741b846e708eb4b40f557a26ed4bb4fa7
HONGWOOYEONG <wooyeunge@gmail.com>
2026-08-25 16:50:55 +0900

    Revert "Merge branch 'branch_suhyun' into branch_hwy"
    This reverts commit 3099136ba1e55af6b5bd48b2ecf6a615dc569130, reversing
    changes made to 53a3b041482d42480d9603f953a780c78f1b1399.

--- Assets/Scripts/Enemy/Boss_2_Puppeteer/IPuppeteerState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IPuppeteerState
-{
-    public void Enter(E_PuppeteerController controller);
-    public void Update(E_PuppeteerController controller);
-    public void Exit(E_PuppeteerController controller);
-}
--- Assets/Scripts/Enemy/PuppeteerFSMState/GroggyState.cs
+++ Assets/Scripts/Enemy/PuppeteerFSMState/GroggyState.cs
@@ -2,14 +2,14 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 //그로기 상태
-public class P_GroggyState : IPuppeteerState
+public class GroggyState : IEnemyState
 {
     [SerializeField] float groggyTime = 5f; //그로기 지속 시간
     [SerializeField] float gravity = 2f; //그로기 상태일 때 원래의 중력에 곱해질 값
     Rigidbody2D rb;
     float nextStateTime = 0;
     float normalGravity;
-    public void Enter(E_PuppeteerController controller)
+    public void Enter(PuppeteerController controller)
     {
         Debug.Log("그로기 상태 진입");
         rb = controller.AddComponent<Rigidbody2D>();
@@ -20,18 +20,18 @@ public class P_GroggyState : IPuppeteerState
         rb.gravityScale = normalGravity * gravity; //그로기 상태일 때 중력값이 크게 작용
     }
 
-    public void Exit(E_PuppeteerController controller)
+    public void Exit(PuppeteerController controller)
     {       
         Debug.Log("그로기 상태 해제");
     }
 
-    public void Update(E_PuppeteerController controller)
+    public void Update(PuppeteerController controller)
     {
-        if (Time.time >= nextStateTime)
+        if (Time.time > nextStateTime)
         {
             nextStateTime = 0f;
             rb.gravityScale = normalGravity; //원래 중력 값으로 복귀
-            controller.ChangeState(controller.idle);
+            controller.ChangeState(new IdleState());
         }
     }
 }
